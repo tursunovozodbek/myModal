c494ef61e1fed3b0b12d5110818d1a6f277a57c4
Ozodbek <tursunovozodbek505@gmail.com>
2023-08-01 15:56:15 +0500

    My model is created by ME HAHAHAHAHA

---
+++ main.js
@@ -0,0 +1,20 @@
+// Vairables
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+// Event
+openBtn.addEventListener("click", function () {
+  modalContainer.style.display = "block";
+  modalContainer.style.color = "skyblue";
+});
+
+closeBtn.addEventListener("click", function () {
+  modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
